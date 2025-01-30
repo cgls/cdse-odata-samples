150c4f312eee30ec9a575ebca924e4db0c85ae77
Stijn Caerts <stijn.caerts@vito.be>
2025-01-30 14:00:06 +0100

    fix SWI footprints

--- scripts/generate_local.sh
+++ scripts/generate_local.sh
@@ -8,5 +8,7 @@ docker image pull $DOCKER_IMAGE
 while IFS="," read -r name path
 do 
     echo $name
-    docker run --rm -a stdout -a stderr -v /data/MTDA/:/data/MTDA/ $DOCKER_IMAGE odata create $path | jq . > $name.json
+    # use VSICACHED option to circumvent libhdf5 writing to stdout
+    # using virtual filesystem with netCDF driver in Docker requires userfaultd system call, so set security-opt to allow it
+    docker run --rm -e VSICACHED=true --security-opt seccomp=unconfined -a stdout -a stderr -v /data/MTDA/:/data/MTDA/ $DOCKER_IMAGE odata create $path | jq . > $name.json
 done < <(tail -n +2 $dir/local.csv)
